bd80b43afa221c17882183885dae7fa725c9cbdf
Dokkabei97 <wkdrn970@naver.com>
2023-07-31 19:50:43 +0900

    feat: api 추가

--- router/router.go
+++ router/router.go
@@ -14,11 +14,20 @@ func SetRouter() *gin.Engine {
 		})
 	}
 
-	kubernetes := router.Group("/api/v1/kuber")
+	kubernetes := router.Group("/api/v1/kube")
 	{
 		kubernetes.GET("", func(context *gin.Context) {
 			context.JSON(200, gin.H{
 				"message": "kubernetes",
+			})
+		})
+	}
+
+	onPremise := router.Group("/api/v1/on-premise")
+	{
+		onPremise.GET("", func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"message": "on-premise",
 			})
 		})
 	}
