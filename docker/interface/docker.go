7e644b1961a25dd5b64278b5828a5b1ff40c6977
Dokkabei97 <wkdrn970@naver.com>
2023-09-01 08:11:08 +0900

    feat: dockerServiceImpl

--- docker/interface/docker.go
+++ docker/interface/docker.go
@@ -1,7 +1,11 @@
 package _interface
 
+// DockerService
+// Image Build, Image Remove, Container Remove 같은 기능은 제공 X
 type DockerService interface {
 	GetContainerList() ([]string, error)
-	GetContainerInfo(containerId string) (string, error)
 	GetContainerLog(containerId string) (string, error)
+	StartContainer(containerId string) (string, error)
+	StopContainer(containerId string) (string, error)
+	RestartContainer(containerId string) (string, error)
 }
--- docker/service/docker.go
+++ docker/service/docker.go
@@ -1,1 +1,41 @@
 package service
+
+import (
+	_interface "JARVIS-agent/docker/interface"
+	"JARVIS-agent/utils"
+)
+
+type dockerService struct {
+	utils utils.Config
+}
+
+func NewDockerService(util utils.Config) _interface.DockerService {
+	return &dockerService{
+		utils: util,
+	}
+}
+
+func (d dockerService) GetContainerList() ([]string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d dockerService) GetContainerLog(containerId string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d dockerService) StartContainer(containerId string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d dockerService) StopContainer(containerId string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d dockerService) RestartContainer(containerId string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
