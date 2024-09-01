2c6aa0455ebe73f72337fb9f8220cead1e5c6f0b
Dokkabei97 <wkdrn970@naver.com>
2024-09-01 22:32:16 +0900

    feat: docker 기능 추가 - 컨테이너 리스트 반환 - 컨테이너 로그 반환 - 이미지 리스트 반환

--- docker/interface/docker.go
+++ docker/interface/docker.go
@@ -1,13 +1,21 @@
 package _interface
 
-// DockerService
-// 1차안에서는 Image Build, Image Remove, Container Remove 같은 기능은 제공 X
+import (
+	"JARVIS-agent/docker"
+	"io"
+)
+
 type DockerService interface {
-	GetContainerList() ([]string, error)
-	GetContainerLog(containerId string) (string, error)
+	GetContainerList() ([]docker.Container, error)
+	GetContainerLog(container docker.Container, follow bool, tail string) (io.ReadCloser, error)
+	RunContainer(imageName string, containerName string, port string, options ...string) (string, error)
 	StartContainer(containerId string) (string, error)
 	StopContainer(containerId string) (string, error)
 	RestartContainer(containerId string) (string, error)
+	RemoveContainer(containerId string, isForce bool) (string, error)
 	PullImage(imageName string) (string, error)
-	GetImageList() ([]string, error)
+	PushImage(imageName string) (string, error)
+	RemoveImage(imageName string, isForce bool) (string, error)
+	GetImageList() ([]docker.Image, error)
+	BuildImage(imageName string, dockerFilePath string) (string, error)
 }
--- docker/service/docker.go
+++ docker/service/docker.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	"JARVIS-agent/docker"
 	_interface "JARVIS-agent/docker/interface"
 	"JARVIS-agent/utils"
 	"context"
 	"github.com/docker/docker/api/types"
+	"io"
 )
 
 type dockerService struct {
@@ -17,53 +19,135 @@ func NewDockerService(util utils.Config) _interface.DockerService {
 	}
 }
 
-func (d dockerService) GetContainerList() ([]string, error) {
-	dockerCli, err := d.utils.GetDockerClient()
+func (ds dockerService) GetContainerList() ([]docker.Container, error) {
+	dockerCli, err := ds.utils.GetDockerClient()
 	if err != nil {
 		return nil, err
 	}
+	defer dockerCli.Close()
 
 	containers, err := dockerCli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	var containerList []string
+	containerList := make([]docker.Container, 0)
 	for _, container := range containers {
-		for _, name := range container.Names {
-			containerList = append(containerList, name)
-		}
+		containerList = append(containerList, docker.Container{
+			Id:   container.ID,
+			Name: container.Names[0],
+			Image: docker.Image{
+				Id:   container.ImageID,
+				Name: container.Image,
+			},
+			Status:  container.Status,
+			Created: container.Created,
+		})
 	}
 
 	return containerList, nil
 }
 
-func (d dockerService) GetContainerLog(containerId string) (string, error) {
+func (ds dockerService) GetContainerLog(container docker.Container, follow bool, tail string) (io.ReadCloser, error) {
+	dockerCli, err := ds.utils.GetDockerClient()
+	if err != nil {
+		return nil, err
+	}
+	defer dockerCli.Close()
+
+	if container.Id == "" || container.Name == "" {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	options := types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     follow,
+		Tail:       tail,
+		Timestamps: true,
+	}
+
+	containerIdORName := container.Id
+	if containerIdORName == "" {
+		containerIdORName = container.Name
+	}
+
+	logs, err := dockerCli.ContainerLogs(ctx, containerIdORName, options)
+	if err != nil {
+		return nil, err
+	}
+	defer logs.Close()
+
+	return logs, nil
+}
+
+func (ds dockerService) RunContainer(imageName string, containerName string, port string, options ...string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (d dockerService) StartContainer(containerId string) (string, error) {
+func (ds dockerService) StartContainer(containerId string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (d dockerService) StopContainer(containerId string) (string, error) {
+func (ds dockerService) StopContainer(containerId string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (d dockerService) RestartContainer(containerId string) (string, error) {
+func (ds dockerService) RestartContainer(containerId string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (d dockerService) PullImage(imageName string) (string, error) {
+func (ds dockerService) RemoveContainer(containerId string, isForce bool) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (d dockerService) GetImageList() ([]string, error) {
+func (ds dockerService) PullImage(imageName string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ds dockerService) PushImage(imageName string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ds dockerService) RemoveImage(imageName string, isForce bool) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ds dockerService) GetImageList() ([]docker.Image, error) {
+	dockerCli, err := ds.utils.GetDockerClient()
+	if err != nil {
+		return nil, err
+	}
+	defer dockerCli.Close()
+
+	images, err := dockerCli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	imageList := make([]docker.Image, 0)
+	for _, image := range images {
+		imageList = append(imageList, docker.Image{
+			Id:      image.ID,
+			Name:    image.RepoTags[0],
+			Size:    image.Size,
+			Created: image.Created,
+		})
+	}
+
+	return imageList, nil
+}
+
+func (ds dockerService) BuildImage(imageName string, dockerFilePath string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
